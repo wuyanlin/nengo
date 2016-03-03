0fb0e890811fb76a11b0358e0ccf9c1e76611d67
arvoelke <arvoelke@gmail.com>
2016-03-02 20:41:14 -0500

    PerfectLIF model with improved performance

--- nengo/neurons.py
+++ nengo/neurons.py
@@ -258,6 +258,40 @@ class LIF(LIFRate):
         refractory_time[spiked > 0] = self.tau_ref + spiketime
 
 
+class PerfectLIF(LIF):
+    """Spiking version of the leaky integrate-and-fire (LIF) neuron model."""
+
+    def step_math(self, dt, J, spiked, voltage, refractory_time):
+        # reduce all refractory times by dt
+        refractory_time -= dt
+
+        # compute effective dt for each neuron, based on remaining time.
+        # note that refractory times that have completed midway into this
+        # timestep will be given a partial timestep, and moreover these will
+        # be subtracted to zero at the next timestep (or reset by a spike)
+        delta_t = (dt - refractory_time).clip(0, dt)
+
+        # update voltage using discretized lowpass filter
+        # since v(t) = v(0) + (J - v(0))*(1 - exp(-t/tau)) assuming
+        # J is constant over the interval [t, t + dt)
+        voltage -= (J - voltage) * np.expm1(-delta_t / self.tau_rc)
+
+        # determine which neurons spiked (set them to 1/dt, else 0)
+        spiked_mask = voltage >= 1
+        spiked[:] = spiked_mask / dt
+        spiked_v = voltage[spiked_mask]
+
+        # set v(0) = 1 and solve for t to compute the spike time
+        t_spike = dt + self.tau_rc * np.log1p(
+            -(spiked_v - 1) / (J[spiked_mask] - 1))
+
+        # set spiked voltages to zero, refractory times to tau_ref, and
+        # rectify negative voltages to a floor of min_voltage
+        voltage[voltage < self.min_voltage] = self.min_voltage
+        voltage[spiked_mask] = 0
+        refractory_time[spiked_mask] = self.tau_ref + t_spike
+
+
 class AdaptiveLIFRate(LIFRate):
     """Adaptive rate version of the LIF neuron model.
 
